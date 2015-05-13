ef89a0f5eddd95a9df2177055635f6c101f33f88
Joe Button <ml_github@joebutton.co.uk>
2015-05-13 21:19:04 +0100

    Tidy and possible fix stuff

--- vse-export/__init__.py
+++ vse-export/__init__.py
@@ -25,19 +25,16 @@ class ArdourVseExport(bpy.types.Operator):
     bl_label = "Export VSE to Ardour"
 
 
-    def get_blender_sequence_data(self):
+    def get_blender_sequence_data(self, context):
         """Build a dict containing the sequencer strip data from Blender, mapped
         by channel (aka "track" / "playlist")"""
         self.blender_sequence_data =  {}
         for s in bpy.context.sequences:
             if s.type == "SOUND":
                 sd = SequenceData()
-                sd.clip_audio_start_frames = s.animation_offset_start
-                sd.clip_audio_end_frames = s.animation_offset_end
-                sd.audio_start_time_frames = s.frame_start - 1
-                sd.audio_duration_frames = s.frame_duration
-                sd.strip_start_time_frames = s.frame_start -1 + s.frame_offset_start
-                sd.strip_end_time_frames = sd.audio_start_time_frames + sd.audio_duration_frames - s.frame_offset_end
+                sd.start_time_frames = s.frame_start + s.frame_offset_start - context.scene.frame_start
+                sd.clip_audio_start_frames = s.animation_offset_start + s.frame_offset_start
+                sd.duration_frames = s.frame_final_duration
                 sd.audio_filename = os.path.basename(s.filepath)
                 sd.audio_file_location = bpy.path.abspath(s.filepath)
                 sd.channel = s.channel
@@ -49,7 +46,7 @@ class ArdourVseExport(bpy.types.Operator):
 
 
     def execute(self, context):
-        self.get_blender_sequence_data()
+        self.get_blender_sequence_data(context)
         ardour_session = ArdourSession()
         spf = 1.0 / context.scene.render.fps # seconds per frame
         for track_no in self.blender_sequence_data.keys():
@@ -61,9 +58,9 @@ class ArdourVseExport(bpy.types.Operator):
                 ardour_session.create_region(
                     strip.audio_file_location,
                     playlist,
-                    strip.strip_start_time_frames * spf,
-                    (strip.strip_start_time_frames - strip.audio_start_time_frames + strip.clip_audio_start_frames) * spf,
-                    (strip.strip_end_time_frames - strip.strip_start_time_frames) * spf,
+                    strip.start_time_frames * spf,
+                    strip.clip_audio_start_frames * spf,
+                    strip.duration_frames * spf,
                     muted = strip.mute,
                 )
         dest = os.path.join(
